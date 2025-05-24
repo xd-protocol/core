830181db93031e810d88d420dcd683f7904e8480
LevX <team@levx.io>
2025-05-25 03:37:03 +0900

    chore: remove StargateVault related code

--- src/interfaces/IStaker.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IStaker {
-    function stake(uint256 amount) external payable;
-
-    function unstake(uint256 amount) external;
-}
