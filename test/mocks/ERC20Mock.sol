0d6cd909c59a3496d4c9345b5c7e58960c76fd88
LevX <team@levx.io>
2025-08-03 16:10:11 +0900

    refactor: update ERC20Mock to use solmate and update forge-std
    - Replace forge-std MockERC20 with solmate ERC20 for consistency
    - Update forge-std submodule to latest version

--- test/mocks/ERC20Mock.sol
+++ test/mocks/ERC20Mock.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: BUSL
 pragma solidity ^0.8.28;
 
-import { MockERC20 } from "forge-std/mocks/MockERC20.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
 
-contract ERC20Mock is MockERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals) {
-        initialize(name, symbol, decimals);
-    }
+contract ERC20Mock is ERC20 {
+    constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol, decimals) { }
 
     function mint(address to, uint256 value) public virtual {
         _mint(to, value);
