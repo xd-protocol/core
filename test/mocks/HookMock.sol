085eeeec55dea9a584b730779c6d34829616d4e8
LevX <team@levx.io>
2025-08-03 23:39:37 +0900

    test: update HookMock to support new settlement callbacks
    - Add structs and storage for tracking new callback calls
    - Implement onMapAccounts, onSettleLiquidity, onSettleTotalLiquidity, onSettleData
    - Add revert flags and getters for each new callback type
    - Enable comprehensive testing of settlement hook behavior

--- test/mocks/HookMock.sol
+++ test/mocks/HookMock.sol
@@ -27,15 +27,50 @@ contract HookMock is IERC20xDHook {
         uint256 timestamp;
     }
 
+    struct MapAccountsCall {
+        uint32 eid;
+        address remoteAccount;
+        address localAccount;
+        uint256 timestamp;
+    }
+
+    struct SettleLiquidityCall {
+        uint32 eid;
+        uint256 timestamp;
+        address account;
+        int256 liquidity;
+    }
+
+    struct SettleTotalLiquidityCall {
+        uint32 eid;
+        uint256 timestamp;
+        int256 totalLiquidity;
+    }
+
+    struct SettleDataCall {
+        uint32 eid;
+        uint256 timestamp;
+        bytes32 key;
+        bytes value;
+    }
+
     InitiateTransferCall[] public initiateTransferCalls;
     GlobalAvailabilityCall[] public globalAvailabilityCalls;
     TransferCall[] public beforeTransferCalls;
     TransferCall[] public afterTransferCalls;
+    MapAccountsCall[] public mapAccountsCalls;
+    SettleLiquidityCall[] public settleLiquidityCalls;
+    SettleTotalLiquidityCall[] public settleTotalLiquidityCalls;
+    SettleDataCall[] public settleDataCalls;
 
     bool public shouldRevertOnInitiate;
     bool public shouldRevertOnGlobalAvailability;
     bool public shouldRevertBeforeTransfer;
     bool public shouldRevertAfterTransfer;
+    bool public shouldRevertOnMapAccounts;
+    bool public shouldRevertOnSettleLiquidity;
+    bool public shouldRevertOnSettleTotalLiquidity;
+    bool public shouldRevertOnSettleData;
 
     string public revertReason = "HookMock: Intentional revert";
 
@@ -90,6 +125,45 @@ contract HookMock is IERC20xDHook {
         afterTransferCalls.push(TransferCall({ from: from, to: to, amount: amount, timestamp: block.timestamp }));
     }
 
+    function onMapAccounts(uint32 eid, address remoteAccount, address localAccount) external override {
+        if (shouldRevertOnMapAccounts) {
+            revert(revertReason);
+        }
+        mapAccountsCalls.push(
+            MapAccountsCall({
+                eid: eid,
+                remoteAccount: remoteAccount,
+                localAccount: localAccount,
+                timestamp: block.timestamp
+            })
+        );
+    }
+
+    function onSettleLiquidity(uint32 eid, uint256 timestamp, address account, int256 liquidity) external override {
+        if (shouldRevertOnSettleLiquidity) {
+            revert(revertReason);
+        }
+        settleLiquidityCalls.push(
+            SettleLiquidityCall({ eid: eid, timestamp: timestamp, account: account, liquidity: liquidity })
+        );
+    }
+
+    function onSettleTotalLiquidity(uint32 eid, uint256 timestamp, int256 totalLiquidity) external override {
+        if (shouldRevertOnSettleTotalLiquidity) {
+            revert(revertReason);
+        }
+        settleTotalLiquidityCalls.push(
+            SettleTotalLiquidityCall({ eid: eid, timestamp: timestamp, totalLiquidity: totalLiquidity })
+        );
+    }
+
+    function onSettleData(uint32 eid, uint256 timestamp, bytes32 key, bytes memory value) external override {
+        if (shouldRevertOnSettleData) {
+            revert(revertReason);
+        }
+        settleDataCalls.push(SettleDataCall({ eid: eid, timestamp: timestamp, key: key, value: value }));
+    }
+
     // Getters for call counts
     function getInitiateTransferCallCount() external view returns (uint256) {
         return initiateTransferCalls.length;
@@ -107,6 +181,22 @@ contract HookMock is IERC20xDHook {
         return afterTransferCalls.length;
     }
 
+    function getMapAccountsCallCount() external view returns (uint256) {
+        return mapAccountsCalls.length;
+    }
+
+    function getSettleLiquidityCallCount() external view returns (uint256) {
+        return settleLiquidityCalls.length;
+    }
+
+    function getSettleTotalLiquidityCallCount() external view returns (uint256) {
+        return settleTotalLiquidityCalls.length;
+    }
+
+    function getSettleDataCallCount() external view returns (uint256) {
+        return settleDataCalls.length;
+    }
+
     // Setters for revert flags
     function setShouldRevertOnInitiate(bool _shouldRevert) external {
         shouldRevertOnInitiate = _shouldRevert;
@@ -122,6 +212,22 @@ contract HookMock is IERC20xDHook {
 
     function setShouldRevertAfterTransfer(bool _shouldRevert) external {
         shouldRevertAfterTransfer = _shouldRevert;
+    }
+
+    function setShouldRevertOnMapAccounts(bool _shouldRevert) external {
+        shouldRevertOnMapAccounts = _shouldRevert;
+    }
+
+    function setShouldRevertOnSettleLiquidity(bool _shouldRevert) external {
+        shouldRevertOnSettleLiquidity = _shouldRevert;
+    }
+
+    function setShouldRevertOnSettleTotalLiquidity(bool _shouldRevert) external {
+        shouldRevertOnSettleTotalLiquidity = _shouldRevert;
+    }
+
+    function setShouldRevertOnSettleData(bool _shouldRevert) external {
+        shouldRevertOnSettleData = _shouldRevert;
     }
 
     function setRevertReason(string memory _reason) external {
